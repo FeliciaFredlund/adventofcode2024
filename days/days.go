ab1397726a4c0ca946db4943f0e3eeb9264fa9a7
FeliciaFredlund <felicia.fredlund@gmail.com>
2025-12-02 21:23:34 +0100

    start on file reading and writing

--- days/days.go
+++ days/days.go
@@ -1,13 +1,39 @@
 package days
 
-// func getFileName(day#, star#, example=false)
-// dXsX.txt / dXsX_example.txt
+import (
+	"errors"
+	"fmt"
+)
 
-// func getFilePath(input/output, filename)
-// ./inputs/filename
-// ./outputs/filename
-// but . is full path
+const (
+	input = iota
+	output
+)
 
-// func saveOutput(data, filename/path, example=false) calls either normally or uses saveExample???
+func getFileName(day int, star int, example bool) string {
+	if example {
+		return fmt.Sprintf("d%ds%d_example.txt", day, star)
+	}
+	return fmt.Sprintf("d%ds%d.txt", day, star)
+}
 
-// func saveExampleOutput(data) makes sure that expected result isn't saved over
+func getFilePath(source int, filename string) (string, error) {
+	var sourceDir string
+	switch source {
+	case input:
+		sourceDir = "input"
+	case output:
+		sourceDir = "output"
+	default:
+		return "", errors.New("source need to be 0 for input and 1 for output, use the enum")
+	}
+	return pathToInputOutput + sourceDir + filename, nil
+}
+
+// func saveOutput(data, filename/path, example) calls either normally or uses saveExample???
+func saveOutput(data []byte, filepath string, example bool) {
+	if example {
+		// do things specific for example, like read in the input file and append the data slice to it
+	}
+	// call on io SaveToFile(data, filepath)
+}
--- io.go
+++ io.go
@@ -3,4 +3,5 @@ package main
 // func getDataFromUrl(url, filename/path) make http request and save to file
 // func getDataFromFile(filename/path) get data from cached file
 // func isCached(filename/path) checks if input have already been cached
+
 // func saveToFile(data, filename/path) to save the data to a file
