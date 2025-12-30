ac3519decf3d063f512c9a9e59565931e40c48ca
FeliciaFredlund <felicia.fredlund@gmail.com>
2025-12-30 12:32:09 +0100

    day 2 complete and cleaned up

--- days/d2.go
+++ days/d2.go
@@ -1,7 +1,6 @@
 package days
 
 import (
-	"fmt"
 	"math"
 	"slices"
 	"strconv"
@@ -24,31 +23,31 @@ func day2star1(data []byte) []byte {
 }
 
 func day2star2(data []byte) []byte {
-	// CLEAN THIS SHIT UP, THE CASE I MISSED WAS A FIRST INCREASE/DECREASE THAT WAS THE OUTLIER
-	// AND THE REST OF THE REPORT WAS THE OTHER
-	// BUT I DON'T THINK I NEED OBEBADREPORTFOUND BUT DELETING IT MADE THINGS BREAK
-
 	stringified := string(data)
+
 	safe := 0
 	for line := range strings.Lines(stringified) {
 		stringNumbers := strings.Fields(line)
-		oneBadReportFound := false
 		isSafe, indexOfUnsafe := lineIsSafe(stringNumbers)
 		if isSafe {
 			safe++
-		} else if !oneBadReportFound {
-			oneBadReportFound = true
-			if isSafe, _ := lineIsSafe(slices.Delete(slices.Clone(stringNumbers), indexOfUnsafe, indexOfUnsafe+1)); isSafe {
-				fmt.Println("removing first")
+			continue
+		}
+		if indexOfUnsafe == -1 {
+			//something went wrong, assume report is unsafe no matter what
+			continue
+		}
+
+		// checking if removing one element would make it a safe report
+		// remove element at left side of unsafe, remove element at right side of unsafe,
+		// if index is 1, check if the unsafe is actually between the first and second element and is inc/dec mismatch
+		if isSafe, _ := lineIsSafe(slices.Delete(slices.Clone(stringNumbers), indexOfUnsafe, indexOfUnsafe+1)); isSafe {
+			safe++
+		} else if isSafe, _ := lineIsSafe(slices.Delete(slices.Clone(stringNumbers), indexOfUnsafe+1, indexOfUnsafe+2)); isSafe {
+			safe++
+		} else if indexOfUnsafe == 1 {
+			if isSafe, _ := lineIsSafe(slices.Delete(slices.Clone(stringNumbers), indexOfUnsafe-1, indexOfUnsafe)); isSafe {
 				safe++
-			} else if isSafe, _ := lineIsSafe(slices.Delete(slices.Clone(stringNumbers), indexOfUnsafe+1, indexOfUnsafe+2)); isSafe {
-				fmt.Println("removing second")
-				safe++
-			} else if indexOfUnsafe > 0 {
-				if isSafe, _ := lineIsSafe(slices.Delete(slices.Clone(stringNumbers), indexOfUnsafe-1, indexOfUnsafe)); isSafe {
-					fmt.Println("removing second")
-					safe++
-				}
 			}
 		}
 	}
@@ -59,7 +58,6 @@ func day2star2(data []byte) []byte {
 func lineIsSafe(line []string) (bool, int) {
 	increasing := false
 	decreasing := false
-	indexOfUnsafe := -1
 	for i := range len(line) {
 		if i+1 == len(line) {
 			return true, -1
@@ -70,8 +68,7 @@ func lineIsSafe(line []string) (bool, int) {
 		difference := math.Abs(float64(currentValue - nextValue))
 
 		if difference == 0 || difference > 3 {
-			indexOfUnsafe = i
-			break
+			return false, i
 		}
 
 		if currentValue < nextValue {
@@ -80,11 +77,10 @@ func lineIsSafe(line []string) (bool, int) {
 			decreasing = true
 		}
 
-		// if a line has both increasing and decreasing or two values are same, break out, this report ain't safe
+		// if a line has both increasing and decreasing, this report ain't safe
 		if increasing && decreasing {
-			indexOfUnsafe = i
-			break
+			return false, i
 		}
 	}
-	return false, indexOfUnsafe
+	return false, -1
 }
